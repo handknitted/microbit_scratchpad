cb59b10dd13c8ec3ef1b79186ac5d8fcf5a033d5
David C Kennedy <david.c.kennedy@hpe.com>
2017-12-05 00:08:08 +0000

    Short, long, single and dual buttons detected

--- buttons.py
+++ buttons.py
@@ -7,58 +7,70 @@ button_b_pressed = "button_b"
 button_together_pressed = "button_together"
 button_long_together_pressed = "button_long_together"
 
-def is_long(conditions):
-    if len(conditions) > 0:
-        time = running_time()
-        print('Time = %s' % time)
-        while running_time() - time < 10000:
-            print('Time = %s, Conditions = %s' %
-                  (time, [condition() for condition in conditions]))
-            if not all(cond() for cond in conditions):
-                return False
-            return True
 
 class ButtonHandler(object):
     
+    presses = []
+    both_button_push_grace_ms = 50
+    long_push_ms = 400
     button_reset = True
+        
+    def button_press(self):
+        if (button_a.is_pressed() or button_b.is_pressed()):
+            if self.button_reset:
+                self.button_reset = False
+                print("Got a button push at %s" % running_time())
+                # delay to catch up if both are being pushed
+                sleep(self.both_button_push_grace_ms)
+                self.identify_button_press()
+        else:
+            self.button_reset = True
     
-    def button_press(self, presses):
+    def identify_button_press(self):
             if (button_a.is_pressed()
                     and button_b.is_pressed()):
-                if self.button_reset:
-                    self.button_reset = False
-                    if is_long([button_a.is_pressed, button_b.is_pressed]):
-                        presses.append(button_long_together_pressed)
-                    else: 
-                        presses.append(button_together_pressed)
+                if self.is_long([button_a.is_pressed, button_b.is_pressed]):
+                    self.presses.append(button_long_together_pressed)
+                else: 
+                    self.presses.append(button_together_pressed)
             elif button_a.is_pressed():
-                if self.button_reset:
-                    self.button_reset = False
-                    if is_long([button_a.is_pressed]):
-                        presses.append(button_long_a_pressed)
-                    else:
-                        presses.append(button_a_pressed)
+                if self.is_long([button_a.is_pressed]):
+                    self.presses.append(button_long_a_pressed)
+                else:
+                    self.presses.append(button_a_pressed)
             elif button_b.is_pressed():
-                if self.button_reset:
-                    self.button_reset = False
-                    if is_long([button_b.is_pressed]):
-                        presses.append(button_long_b_pressed)
-                    else:
-                        presses.append(button_b_pressed)
-            else:
-                self.button_reset = True
+                if self.is_long([button_b.is_pressed]):
+                    self.presses.append(button_long_b_pressed)
+                else:
+                    self.presses.append(button_b_pressed)
+
+    def is_long(self, conditions):
+        if len(conditions) > 0:
+            time = running_time()
+            print('Time = %s' % time)
+            while running_time() - time < self.long_push_ms:
+                print('Time = %s, Conditions = %s' %
+                      (running_time(), 
+                       [condition() for condition in conditions]))
+                if not all(cond() for cond in conditions):
+                    print("short")
+                    return False
+            print("long")
+            return True
+    
+    def get_presses(self):
+        return self.presses
 
 
 print(running_time())
 sleep(1000)
 print(running_time())
 count = 10
-inputs = []
 handle_buttons = ButtonHandler()
-while len(inputs) < count:
-    handle_buttons.button_press(inputs)
+while len(handle_buttons.get_presses()) < count:
+    handle_buttons.button_press()
     
-print(inputs)
+print(handle_buttons.get_presses())
     
 
         
