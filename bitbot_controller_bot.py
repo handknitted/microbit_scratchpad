e9ce2bc1108687e88d8a95612e4f63a4f9c2e1e9
David C Kennedy <darfed@gmail.com>
2019-03-20 22:33:57 +0000

    Added reverse for motors by tilting

--- bitbot_controller_bot.py
+++ bitbot_controller_bot.py
@@ -11,8 +11,10 @@ MOTOR_OFF = 0
 MOTOR_ONE = 'one'
 MOTOR_TWO = 'two'
 
-MOTOR_ONE_WRITE = pin16.write_digital
-MOTOR_TWO_WRITE = pin12.write_digital
+MOTOR_ONE_POS_WRITE = pin16.write_digital
+MOTOR_ONE_NEG_WRITE = pin0.write_digital
+MOTOR_TWO_POS_WRITE = pin12.write_digital
+MOTOR_TWO_NEG_WRITE = pin8.write_digital
 
 while True:
     transmitted_state = radio.receive()
@@ -24,16 +26,21 @@ while True:
             motor_one_state = int(state[1])
             # MOTOR_ONE_WRITE(motor_one_state)
             motor_two_state = int(state[3])
+            direction_modifier = int(state[4])
             # MOTOR_TWO_WRITE(motor_two_state)
             if int(state[1]):
-                MOTOR_ONE_WRITE(MOTOR_ON)
+                MOTOR_ONE_POS_WRITE((MOTOR_ON - direction_modifier)**2)
+                MOTOR_ONE_NEG_WRITE((MOTOR_OFF - direction_modifier)**2)
             else:
-                MOTOR_ONE_WRITE(MOTOR_OFF)
+                MOTOR_ONE_POS_WRITE(MOTOR_OFF)
+                MOTOR_ONE_NEG_WRITE(MOTOR_OFF)
             if int(state[3]):
-                MOTOR_TWO_WRITE(MOTOR_ON)
+                MOTOR_TWO_POS_WRITE((MOTOR_ON - direction_modifier)**2)
+                MOTOR_TWO_NEG_WRITE((MOTOR_OFF - direction_modifier)**2)
             else:
-                MOTOR_TWO_WRITE(MOTOR_OFF)
+                MOTOR_TWO_POS_WRITE(MOTOR_OFF)
+                MOTOR_TWO_NEG_WRITE(MOTOR_OFF)
 
         except Exception:
-            MOTOR_TWO_WRITE(MOTOR_OFF)
-            MOTOR_ONE_WRITE(MOTOR_OFF)
+            MOTOR_TWO_POS_WRITE(MOTOR_OFF)
+            MOTOR_ONE_POS_WRITE(MOTOR_OFF)
--- bitbot_controller_handset.py
+++ bitbot_controller_handset.py
@@ -2,6 +2,9 @@
 from microbit import *
 import radio
 
+FORWARD = 0
+BACKWARD = 1
+
 MOTOR_ON = 1
 MOTOR_OFF = 0
 
@@ -18,13 +21,25 @@ _state = 'S'
 radio.on()
 radio.config(channel=47, power=5)
 
+
+def get_tilt():
+    reading = accelerometer.get_y()
+    if reading > 750:
+        return BACKWARD
+    return FORWARD
+
+
 while True:
-    state = '{:s},{},{:s},{}'.format(MOTOR_ONE, int(button_a.is_pressed()), MOTOR_TWO, int(button_b.is_pressed()))
+    direction = get_tilt()
+    state = '{:s},{},{:s},{},{}'.format(
+        MOTOR_ONE, int(button_a.is_pressed()),
+        MOTOR_TWO, int(button_b.is_pressed()),
+        direction)
     state_split = state.split(',')
     display.clear()
     if int(state_split[1]):
-        display.set_pixel(0, 0, 9)
+        display.set_pixel(0, 4 * direction, 9)
     if int(state_split[3]):
-        display.set_pixel(4, 0, 9)
+        display.set_pixel(4, 4 * direction, 9)
     radio.send(state)
     sleep(1)
